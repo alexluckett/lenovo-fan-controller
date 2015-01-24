4950dceb6484d9ac63e529714ba9a1e1f305696d
Alex Luckett <alexgluckett@gmail.com>
2015-01-24 00:26:11 +0000

    Autodetect functionality for fan_mode control file

--- FanControl/fileWriter.py
+++ FanControl/fileWriter.py
@@ -1,14 +1,46 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
+from argparse import ArgumentError
 
-FAN_MODE_FILE = '/sys/devices/pci0000:00/0000:00:1f.0/PNP0C09:00/VPC2004:00/fan_mode' #TODO find a way to auto detect this
+autoDetectEnabled = True
+FAN_MODE_FILENAME = 'fan_mode' # name of control file
+FAN_MODE_FULLPATH = '/sys/devices/pci0000:00/0000:00:1f.0/PNP0C09:00/VPC2004:00/fan_mode' # path on lenovo z570 (developer)
 
-f = open(FAN_MODE_FILE, 'w')
+# lists all possible fan_mode control files
+def listFanModeFiles():
+    searchResults = []
+    
+    for root, dirs, files in os.walk('/sys/devices/'):
+        if FAN_MODE_FILENAME in files:
+            searchResults.append(os.path.join(root, FAN_MODE_FILENAME))
+            
+    return searchResults
 
-mode = sys.argv[1] # retrieve mode argument
+def autoDetect():
+    global FAN_MODE_FULLPATH
+    
+    modes = listFanModeFiles()
 
-if(not mode.isdigit()):
-    raise TypeError('Second argument must be numerical.')
+    if(len(modes) == 1): # auto detect only possible if we know the one file to use. else up to user to define.
+        FAN_MODE_FULLPATH = modes[0]
+        print 'Auto detected file_mode path:', FAN_MODE_FULLPATH
+    else:
+        print 'Could not auto detect file_mode path. Using user specified:', FAN_MODE_FULLPATH
 
-f.write(mode) 
+
+if(autoDetectEnabled):
+    autoDetect()
+
+f = open(FAN_MODE_FULLPATH, 'w')
+
+try:
+    mode = sys.argv[1] # retrieve mode argument
+    
+    if(not mode.isdigit()):
+        raise TypeError('Second argument must be numerical.')
+    
+    f.write(mode)
+except IndexError:
+    raise ArgumentError('No second argument to use as fan mode')
+
--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Provided as-is without support, guarantees or warranty. Use entirely at your own
 Developed for a Lenovo z570 running Ubuntu 14.10 w/ Unity. May work on other Lenovo Ideapad systems - see the 'Set up' section.
 
 ## Setup
+This script should auto detect the `fan_mode` file used by your operating system to set the fan speeds. Toggle it on/off by setting `autoDetectEnabled` in fileWriter to either True or False. If false, it will revert to the variable `FAN_MODE_FILE`.
+
 `'FAN_MODE_FILE'` in fileWriter may need to be altered to point to the location of your fan_mode file. This can be found by running the following command in a terminal:
 
 `find /sys/devices -name "fan_mode"`
